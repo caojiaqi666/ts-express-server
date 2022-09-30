c8ad4b0140ac28bbf6d61da8da023f429081934c
anjiang <1508771379@qq.com>
2022-09-30 11:09:30 +0800

    feat: 添加login接口

--- src/control/user/index.ts
+++ src/control/user/index.ts
@@ -1,1 +1,36 @@
 const userModel = require("../Model");
+
+const login = async (ctx: any) => {
+	const { username, passwd } = ctx.request.body;
+	console.log("username, passwd: ", username, passwd);
+	return (ctx.body = {
+		state: 0,
+		msg: "登录成功"
+	});
+	let user = await UserModel.findOne({ username });
+	if (!user) {
+		// 如果用户名还没被使用
+		// 重定向到聊天页面
+		return (ctx.body = {
+			state: 1,
+			msg: "用户名不存在"
+		});
+	} else {
+		if (passwd !== user.passwd) {
+			return (ctx.body = {
+				state: 2,
+				msg: "密码错误"
+			});
+		}
+		console.log(`${user.username}登录成功`);
+		return (ctx.body = {
+			state: 0,
+			msg: "登录成功",
+			user
+		});
+	}
+};
+
+module.exports = {
+	login
+};
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,6 @@
-// src/routes/index.ts
-
 import { Express, Request, Response, Router } from "express";
 import commonRes from "../utils/commonRes";
+const { login } = require("../Model/user");
 
 // 路由配置接口
 interface RouterConf {
@@ -13,11 +12,15 @@ interface RouterConf {
 // 路由配置
 // const routerConf: Array<RouterConf> = [];
 
-function routes(app: Express) {
+const routes = (app: Express) => {
 	// 根目录
 	app.get("/", (req: Request, res: Response) => {
-		commonRes(res, { word: "Hello Shinp!!!" }); // 成功
+		commonRes(res, { word: "Hello Shinp!!!" });
 	});
-}
+
+	app.post("/login", (req: Request, res: Response) => {
+		commonRes(res, { word: "Hello Shinp77777!!!" });
+	});
+};
 
 export default routes;
